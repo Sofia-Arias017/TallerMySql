4b977eaeba69742b2721a0a33bbb1a685116de0d
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-06-03 15:49:45 -0500

    feat: :sparkles: empleados que tienen un salario superior a $2.500.000, mostrando su nombre, puesto y salario.

--- ConsultasBasicas.sql
+++ ConsultasBasicas.sql
@@ -5,3 +5,8 @@ WHERE ciudad = 'Madrid';
 
 SELECT nombre, precio FROM productos
 WHERE precio>100000;
+
+SELECT usuarios.nombre, empleados.puesto,empleados.salario
+FROM usuarios
+JOIN empleados ON usuarios.usuario_id = empleados.usuario_id
+WHERE empleados.salario > 2500000;
