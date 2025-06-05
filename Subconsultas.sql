25106e9632eea81df9d34b0e3b204db5b24dd0f0
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-06-05 18:19:57 -0500

    feat: :sparkles: Nombres de los clientes que han realizado al menos un pedido de más de $500.000.

---
+++ Subconsultas.sql
@@ -0,0 +1,13 @@
+-- 1.Encuentra los nombres de los clientes que han
+-- realizado al menos un pedido de más de $500.000.
+
+SELECT nombre
+FROM usuarios
+WHERE usuario_id IN (
+    SELECT pedidos.cliente_id
+    FROM pedidos
+    JOIN detalles_pedidos
+        ON pedidos.pedido_id = detalles_pedidos.pedido_id
+    GROUP BY pedidos.cliente_id
+    HAVING SUM(detalles_pedidos.precio_unitario * detalles_pedidos.cantidad) > 500000
+);
